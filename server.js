240796a7acc1937590a2fcf7a75ad743aa2d0ca9
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2020-12-10 12:36:54 -0500

    New ptorject to test Leaflet.

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 8080;
+
+fs.readFile('./Leaflet-test/index.html', (err, html) => {
+
+    if (err) throw err;
+
+    http.createServer((request, response) => {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(PORT);
+});
